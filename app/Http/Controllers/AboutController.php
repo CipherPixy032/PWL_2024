68940a27bf3b65ad6d52773cebaba466592e9d69
CipherPixy032 <mollym3gtr@gmail.com>
2025-02-24 21:27:33 +0700

    Commit praktikum 1 part 2

---
+++ app/Http/Controllers/AboutController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AboutController extends Controller
+{
+    public function __invoke()
+    {
+        return 'NIM: 2341720190<br>Nama: Krisna Prawira Nugraha';
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,47 +1,16 @@
 <?php
 
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('/', HomeController::class);
+Route::get('/about', AboutController::class);
+Route::get('/articles/{id}', ArticleController::class);
 
-Route::get('/hello', function () {
-    return 'Hello World';
-});
+use App\Http\Controllers\PhotoController;
 
-Route::get('/world', function () {
-    return 'World';
-});
-
-Route::get('/welcome', function () {
-    return 'Selamat datang';
-});
-
-Route::get('/about', function () {
-    return 'NIM: 2341720190<br>Nama: Krisna Prawira Nugraha';
-});
-
-Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
-    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
-});
-
-Route::get('/articles/{id}', function ($id) {
-    return "Halaman Artikel dengan ID " . $id;
-});
-
-Route::get('/user/{name?}', function ($name='John') { return 'Nama saya '.$name;
-});
-
-Route::get('/user/profile', function() {
-//
-})->name('profile');
-
-
+Route::resource('photos', PhotoController::class);
+Route::resource('photos', PhotoController::class)->only(['index', 'show']);
+Route::resource('photos', PhotoController::class)->except(['create', 'store', 'update', 'destroy']);

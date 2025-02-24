f726e324c78022f7eb02e70f3fabc6eab72c19cc
CipherPixy032 <mollym3gtr@gmail.com>
2025-02-24 20:02:05 +0700

    Commit praktikum 1 part 1

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,35 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/hello', function () {
+    return 'Hello World';
 });
+
+Route::get('/world', function () {
+    return 'World';
+});
+
+Route::get('/welcome', function () {
+    return 'Selamat datang';
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341720190<br>Nama: Krisna Prawira Nugraha';
+});
+
+Route::get('/posts/{post}/comments/{comment}', function ($postId, $commentId) {
+    return 'Pos ke-'.$postId." Komentar ke-: ".$commentId;
+});
+
+Route::get('/articles/{id}', function ($id) {
+    return "Halaman Artikel dengan ID " . $id;
+});
+
+Route::get('/user/{name?}', function ($name='John') { return 'Nama saya '.$name;
+});
+
+Route::get('/user/profile', function() {
+//
+})->name('profile');
+
+
